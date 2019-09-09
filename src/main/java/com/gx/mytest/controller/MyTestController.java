493b720e0456043d9e2b93d7afbab04e66889714
gx1360414553 <1360414553@qq.com>
2019-09-09 09:37:38 +0800

    二维码展示

--- src/main/java/com/gx/mytest/controller/MyTestController.java
+++ src/main/java/com/gx/mytest/controller/MyTestController.java
@@ -5,17 +5,17 @@ import com.google.zxing.common.BitMatrix;
 import com.gx.qrcode.QRCodeUtil;
 import com.gx.qrcode.ZXingBackGroundUtils;
 import com.gx.shorturl.ShortNetAddressUtil;
-import com.gx.shorturl.ShortUrl;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ClassUtils;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 /**
  * @author 高雄
